6dffe86681bb1c924335968854c792d638f42855
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2019-05-06 16:47:49 +0100

    now working

--- app/src/main/java/com/martinwalls/sudokusolver/MainActivity.java
+++ app/src/main/java/com/martinwalls/sudokusolver/MainActivity.java
@@ -1,9 +1,11 @@
 package com.martinwalls.sudokusolver;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -13,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
 //    private ProgressBar progressBar;
     private ProgressDialog progressDialog;
-    private GridLayout gridLayout;
-    private Board boardDisplay;
+    private GridLayout sudokuGrid;
+
+    private TextView focusedSquare;
 
 
     @Override
@@ -22,106 +25,110 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gridLayout = findViewById(R.id.sudoku_grid);
+        sudokuGrid = findViewById(R.id.sudoku_grid);
 
-        /*
-        Button solveBtn = findViewById(R.id.btn_solve);
-        solveBtn.setOnClickListener(new View.OnClickListener() {
+        GridLayout keypadGrid = findViewById(R.id.keypad);
+
+        Button clearButton = keypadGrid.findViewById(R.id.button_clear);
+        clearButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showProgressDialog();
-                new Solver().start();
+                clearFocusedSquare();
             }
         });
 
-        Button resetBtn = findViewById(R.id.btn_reset);
-        resetBtn.setOnClickListener(new View.OnClickListener() {
+        Button resetButton = keypadGrid.findViewById(R.id.button_reset);
+        resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resetBoard();
             }
-        });*/
+        });
+
+        Button solveButton = keypadGrid.findViewById(R.id.button_solve);
+        solveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Solver().start();
+            }
+        });
+
     }
 
-//    public void squareOnClick(View view) {
-//        view.requestFocus();
-//    }
-
-//        ProgressDialog progressDialog = new ProgressDialog(this);
-//        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-//        progressDialog.setMessage("Progress dialog");
-//        progressDialog.setCancelable(false);
-//        progressDialog.setMax(100);
-//        progressDialog.show();
-
-
-//        progressBar = findViewById(R.id.progress_bar);
-
-//        new Thread(new Runnable() {
-//            @Override
-//            public void run() {
-//                while (progressBar.getProgress() < 100) {
-//                    progressBar.incrementProgressBy(1);
-//                    try {
-//                        Thread.sleep(50);
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            }
-//        }).start();
-
-//        new Solver().start();
-//    }
-
-//    private void showProgressDialog() {
-//        progressDialog = new ProgressDialog(this);
-//        progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-//        progressDialog.setMessage("Solving...");
-//        progressDialog.setCancelable(false);
-//        progressDialog.setMax(100);
-//        progressDialog.show();
-//    }
-
-    private void showProgressDialog() {
-        progressDialog = new ProgressDialog();
-
-        progressDialog.show(getSupportFragmentManager(), "progress");
-    }
-
-    private void setOriginalSquaresBold(List<int[]> squares) {
-        for (int[] square : squares) {
-            EditText editText = gridLayout.findViewById(
-                    getResources().getIdentifier("s" + square[0] + square[1],
-                            "id", getPackageName()));
-            editText.setTypeface(editText.getTypeface(), Typeface.BOLD);
+    public void keypadNumberOnClick(View view) {
+        TextView focusedView = (TextView) getCurrentFocus();
+        if (focusedView != null) {
+            focusedView.setText(((Button) view).getText().toString());
         }
     }
 
-    private void updateGrid(Board board) {
-        for (int row = 0; row < Board.BOARDSIZE; row++) {
-            for (int col = 0; col < Board.BOARDSIZE; col++) {
-                EditText editText = gridLayout.findViewById(
-                        getResources().getIdentifier("s"+row+col, "id", getPackageName()));
-                int squareVal = board.getSquare(row, col);
-                if (squareVal == 0) {
-                    editText.setText("");
-                } else {
-                    editText.setText(String.valueOf(squareVal));
+    public void clearFocusedSquare() {
+        TextView focusedView = (TextView) getCurrentFocus();
+        if (focusedView != null) {
+            focusedView.setText(getString(R.string.blank_square));
+        }
+    }
+
+    private void showProgressDialog() {
+        progressDialog = new ProgressDialog();
+        progressDialog.show(getSupportFragmentManager(), "progress");
+    }
+
+    //TODO
+    private void setOriginalSquares(List<int[]> originalSquares) {
+        for (int i = 0; i < sudokuGrid.getChildCount(); i++) {
+            GridLayout box = (GridLayout) sudokuGrid.getChildAt(i);
+            for (int j = 0; j < box.getChildCount(); j++) {
+                int row = (i/3) * 3 + j/3;
+                int col = (i%3) * 3 + j%3;
+                for (int[] originalSquare : originalSquares) {
+                    if (row == originalSquare[0] && col == originalSquare[1]) {
+                        TextView square = (TextView) box.getChildAt(j);
+                        square.setBackground(getDrawable(R.drawable.sudoku_cell_original));
+                    }
                 }
             }
         }
     }
 
+    private void clearFocus() {
+        View focused = getCurrentFocus();
+        if (focused != null) {
+            focused.clearFocus();
+        }
+    }
+
     private void resetBoard() {
-        for (int row = 0; row < Board.BOARDSIZE; row++) {
-            for (int col = 0; col < Board.BOARDSIZE; col++) {
-                EditText editText = gridLayout.findViewById(
-                        getResources().getIdentifier("s" + row + col, "id", getPackageName()));
-                editText.setTypeface(editText.getTypeface(), Typeface.NORMAL); //FIXME
-                editText.setText("");
+        for (int i = 0; i < sudokuGrid.getChildCount(); i++) {
+            GridLayout box = (GridLayout) sudokuGrid.getChildAt(i);
+            for (int j = 0; j < box.getChildCount(); j++) {
+                TextView square = (TextView) box.getChildAt(j);
+                square.setText(getString(R.string.blank_square));
+                square.setBackground(getDrawable(R.drawable.sudoku_cell));
+                square.setFocusableInTouchMode(true);
             }
         }
+        clearFocus();
+    }
+
+    private void updateGridToSolution(Board board) {
+        for (int i = 0; i < sudokuGrid.getChildCount(); i++) {
+            GridLayout box = (GridLayout) sudokuGrid.getChildAt(i);
+            for (int j = 0; j < box.getChildCount(); j++) {
+                TextView square = (TextView) box.getChildAt(j);
+                int row = (i/3) * 3 + j/3;
+                int col = (i%3) * 3 + j%3;
+
+                int val = board.getSquare(row, col);
+                if (val == Board.BLANK) {
+                    square.setText(getString(R.string.blank_square));
+                } else {
+                    square.setText(String.valueOf(val));
+                }
+                square.setFocusableInTouchMode(false);
+            }
+        }
+        clearFocus();
     }
 
     private class Solver extends Thread {
@@ -132,45 +139,59 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void run() {
-            getBoard();
-            squaresToSolve = board.getBlankSquares();
-            originalSquares = board.getOriginalSquares();
-            MainActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    setOriginalSquaresBold(originalSquares);
+            if (getBoard()) {
+                squaresToSolve = board.getBlankSquares();
+                originalSquares = board.getOriginalSquares();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        setOriginalSquares(originalSquares);
+                    }
+                });
 
-                }
-            });
+                int pointer = 0;
+                iterValsFor(pointer);
 
-            int pointer = 0;
-            iterValsFor(pointer);
-
-            MainActivity.this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    updateGrid(board);
-                    progressDialog.dismiss();
-                }
-            });
-
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        updateGridToSolution(board);
+//                        progressDialog.dismiss();
+                    }
+                });
+            } else {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Error: unsolvable grid", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         }
 
-        private void getBoard() {
-            for (int row = 0; row < Board.BOARDSIZE; row++) {
-                for (int col = 0; col < Board.BOARDSIZE; col++) {
-                    EditText editText = gridLayout.findViewById(
-                            getResources().getIdentifier("s"+row+col, "id", getPackageName()));
-                    String valStr = editText.getText().toString();
+        private boolean getBoard() {
+            for (int i = 0; i < sudokuGrid.getChildCount(); i++) {
+                GridLayout box = (GridLayout) sudokuGrid.getChildAt(i);
+                for (int j = 0; j < box.getChildCount(); j++) {
+                    int row = (i/3) * 3 + j/3;
+                    int col = (i%3) * 3 + j%3;
+                    TextView square = (TextView) box.getChildAt(j);
+                    String valString = square.getText().toString();
                     int val;
-                    if (valStr.equals("")) {
+                    if (valString.equals(getString(R.string.blank_square))) {
                         val = Board.BLANK;
+                        board.setSquare(row, col, val);
                     } else {
-                        val = Integer.parseInt(valStr);
+                        val = Integer.parseInt(valString);
+                        if (isDigitValid(val, row, col)) {
+                            board.setSquare(row, col, val);
+                        } else {
+                            return false;
+                        }
                     }
-                    board.setSquare(row, col, val);
                 }
             }
+            return true;
         }
 
         private boolean isValInRow(int val, int row) {
